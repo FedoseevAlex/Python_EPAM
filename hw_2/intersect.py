508f23b1691b366701738dc94d632f4091b1dddc
fag <fedoseevalex@inbox.ru>
2018-03-31 19:18:46 +0300

    Corrections after checking

--- hw_2/intersect.py
+++ hw_2/intersect.py
@@ -1,17 +1,22 @@
 def intersect(*args):
     res = []
-    for x in args[0]:
-        if x in res:
-            continue
-        for y in args[1:]:
-            if x not in y:
-                break
+    if args:
+        for x in args[0]:
+            if x in res:
+                continue
+            for y in args[1:]:
+                if x not in y:
+                    break
             else:
                 res.append(x)
-    print(res)
-    return res
+        print(res)
+        return res
 
-print("Result of intersect(['s','c','a','m'], ['s','r','a','m'])")
-intersect(['s','c','a','m'], ['s','r','a','m'])
-print("Result of intersect(('a','b','c',1,2,3,4,'am'),['sp','am','a',1,2])")
-intersect(('a','b','c',1,2,3,4,'am'),['sp','am','a',1,2])
+#print("Result of intersect(['s','c','a','m'], ['s','r','a','m'])")
+#intersect(['s','c','a','m'], ['s','r','a','m'])
+#print("Result of intersect(('a','b','c',1,2,3,4,'am'),['sp','am','a',1,2])")
+#intersect(('a','b','c',1,2,3,4,'am'),['sp','am','a',1,2])
+intersect([1,2,3,4], [4,5,3,5], [6,7,8])
+intersect([1,2,3,4], [4,5,3,5], [6,7,8], ('a','b','c',1,2,3,4,'am'),['sp','am','a',1,2])
+intersect('1212143')
+intersect()
