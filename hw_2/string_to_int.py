54cdc6c9443f90a1b60001a1378fa73225fd5d83
fag <fedoseevalex@inbox.ru>
2018-03-31 19:19:03 +0300

    Corrections after checking

--- hw_2/string_to_int.py
+++ hw_2/string_to_int.py
@@ -1,30 +1,34 @@
 def strtoint(string):
-  res = 0
-  for i in string:
-    if ord(i) < 10:
-      res *= 10
-    elif ord(i) < 100:
-      res *= 100
-    elif ord(i) < 1000:
-      res *= 1000
-    res += ord(i)
-  print(res)
-  return res
+    if string:
+        res = 0
+        for i in string:
+          if ord(i) < 10:
+            res *= 10
+          elif ord(i) < 100:
+            res *= 100
+          elif ord(i) < 1000:
+            res*=1000
+          res += ord(i)
+        print(res)
+        return res
 
 print("Result strtoint('abcd'): ")
 strtoint('abcd')
+strtoint('')
 
 # Вариант без свитча
 def strtoint2(string):
-    res = 0
-    pow = 0
-    for i in string:
-        while(ord(i) // 10**pow):
-            pow += 1
-        res *= 10**pow
-        res += ord(i)
-    print(res)
-    return res
+    if string:
+        res = 0
+        pow = 0
+        for i in string:
+            while(ord(i) // 10**pow):
+                pow += 1
+            res *= 10**pow
+            res += ord(i)
+        print(res)
+        return res
 
 print("Result strtoint2('abcd'): ")
 strtoint2('abcd')
+strtoint2('')
