d387f80b57bc2e39029f0cfe48e2613b4abe2f47
fag <fedoseevalex@inbox.ru>
2018-04-12 17:22:55 +0300

    HW L6.1

--- hw_6/Currency.py
+++ hw_6/Currency.py
@@ -4,57 +4,80 @@
 import abc
 import decimal
 
-class PositiveDecimal:
 
+class PositiveDecimal:
     def __init__(self):
-        self.label = 'Course <{}>'.format(id(self))
+        self.label = '<Course at {}>'.format(id(self))
 
     def __get__(self, instance, owner):
-        try:
-            return instance.__dict__[self.label]
-        except KeyError:
+        if instance is None:
             return self
+        else:
+            try:
+                return instance.__dict__[self.label]
+            except KeyError:
+                return self
 
     def __set__(self, instance, value):
-        if isinstance(value, (int, float)):
-            if value < 0:
-                raise ValueError('Must be positive.')
-            instance.__dict__[self.label] = decimal.Decimal(str(value))
+        try:
+            to_set = decimal.Decimal(str(value))
+        except decimal.InvalidOperation:
+            raise ValueError('Wrong attribute value')
+        if to_set < 0:
+            raise ValueError('Negative values not allowed.')
         else:
-            raise ValueError('Wrong value for money.')
+            instance.__dict__[self.label] = to_set
+
 
 class Currency(abc.ABC):
-    _amount = PositiveDecimal()
     course = PositiveDecimal()
-    symbol = None
+    symbol = '¤'
 
     def __init__(self, amount):
-        self._amount = amount
+        self.amount = amount
+
+    def __str__(self):
+        return '{} {}'.format(self.amount, self.symbol)
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value) -> None:
+        try:
+            to_set = decimal.Decimal(str(value))
+        except decimal.InvalidOperation:
+            raise ValueError('Wrong attribute value.')
+        if to_set < 0:
+            raise ValueError('Negative values not allowed.')
+        else:
+            self._amount = to_set
 
     def to(self, other):
-         return self._amount * (1 / self.course) * other._amount
+        return '{} {}'.format(self.amount * other.course / self.course, other.symbol)
+
 
 
 class Dollar(Currency):
-    #course = 2
     symbol = '$'
-
-class Rubble(Currency):
-    course = PositiveDecimal()
-    symbol = 'R'
+    pass
 
 class Euro(Currency):
-    course = PositiveDecimal()
     symbol = '€'
+    pass
 
-a = Dollar(3)
-print(a.course)
+class Ruble(Currency):
+    symbol = 'R'
+    pass
 
-b = Rubble(192)
-
-
-c = Euro(1)
-
-# print(b.to(Dollar))
-# print(b.to(Rubble))
-# print(b.to(Euro))
+d = Dollar(234)
+r = Ruble(240)
+print(d)
+print(d.amount)
+print(d.course)
+Dollar.course = 1
+Ruble.course = -60
+Euro.course = -9
+r.to(Dollar)
+print(r.to(Euro))
