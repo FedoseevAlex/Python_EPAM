dd4ef61285e5c5bd96b97bed7a06cbeec30df6ed
fag <fedoseevalex@inbox.ru>
2018-04-16 23:41:55 +0300

    Small corrections

--- hw_9/ContextManager.py
+++ hw_9/ContextManager.py
@@ -3,29 +3,30 @@
 import time
 import traceback
 
+
 class LogIt:
     def __init__(self, logfile):
         self.logfile = logfile
 
     def __enter__(self):
-        self.fd = open(self.logfile, 'w')
         self.start_time = time.time()
-        return self.fd
 
     def __exit__(self, e_type, e_val, e_tb):
         if e_type is not None:
+            self.fd = open(self.logfile, 'w')
             self.elapsed_time = time.time() - self.start_time
             self.now = time.strftime('%d.%m.%Y at %H:%M')
             print('Error occured {}:'.format(self.now), file=self.fd)
             print('Run time: {} s'.format(self.elapsed_time), file=self.fd)
             traceback.print_exception(e_type, e_val, e_tb, file=self.fd)
-        self.fd.close()
+            self.fd.close()
+
 
 def foo():
     a = 0
     for i in range(200):
         a += (i ** i) ** i
-    print(a)
+    raise ArithmeticError('WOW')
     return a
 
 if __name__ == '__main__':
