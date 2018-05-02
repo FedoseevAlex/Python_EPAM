7b509cb790966645c2e1d3281bf24315f3a74d78
FedoseevAlex <fedoseevalex@inbox.ru>
2018-05-02 19:08:40 +0300

    Add unittest

--- hw_11/triangle_area.py
+++ hw_11/triangle_area.py
@@ -2,68 +2,298 @@
 # HW #L11.2 Triangle area
 
 """
-Triangle area module.
+This module contains Point class and Triangle class.
+Point defined by two in or float values.
+Triangle is defined by three Point objects.
 """
 
 import math
-from itertools import combinations as cb
+from functools import partial
+
+
+class Point:
+    """
+    This class represents a point in 2D. Thus point accepts exactly two arguments 'x' and 'y'.
+    If no arguments were given during an instantiation the (0, 0) point will be created.
+    Note: Point is immutable! After creating of instance it is not possible to change point coordinates.
+    Example:
+
+    >>> a = Point(0, 1)
+    >>> a
+    (0.0, 1.0)
+    >>> b = Point(0, 4)
+    >>> a.distance(b)
+    3.0
+    >>> b.distance()
+    4.0
+    """
+
+    def __init__(self, x=0, y=0):
+        """
+        Initialise new Point object with provided coordinates.
+        :param x: Coordinate on X axis
+        :type x: int or float. Any given value will be cast to float. If no value given x = 0.
+        :param y: Coordinate on X axis
+        :type y: int or float. Any given value will be cast to float. If no value given y = 0.
+
+        Example:
+        >>> a = Point(x=123)
+        >>> a
+        (123.0, 0.0)
+        >>> a = Point(y=392.3)
+        >>> a
+        (0.0, 392.3)
+        >>> b = Point(12, '34')
+        Traceback (most recent call last):
+        ...
+        ValueError: Wrong value to define point. Both x and y must be int or float.
+        >>> c = Point()
+        >>> c
+        (0.0, 0.0)
+        """
+        if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+            self.__x = float(x)
+            self.__y = float(y)
+        else:
+            raise ValueError('Wrong value to define point. Both x and y must be int or float.')
+
+    def __repr__(self):
+        return '({}, {})'.format(self.x, self.y)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    @property
+    def x(self) -> float:
+        """
+        Property that returns x coordinate of point.
+        Note: Setter and deleter not defined for x. So it can not be changed or deleted.
+        Examples:
+        >>> a = Point(42.134, 1.01)
+        >>> a.x
+        42.134
+        >>> a.x = 0
+        Traceback (most recent call last):
+        ...
+        AttributeError: can't set attribute
+        >>> del a.x
+        Traceback (most recent call last):
+        ...
+        AttributeError: can't delete attribute
+
+        :return: coordinate of type float
+        """
+        return self.__x
+
+    @property
+    def y(self) -> float:
+        """
+        Property that returns x coordinate of point.
+        Note: Setter and deleter not defined for y. So it can not be changed or deleted.
+        Examples:
+        >>> a = Point(42.134, 1.01)
+        >>> a.y
+        1.01
+        >>> a.y = 45
+        Traceback (most recent call last):
+        ...
+        AttributeError: can't set attribute
+        >>> del a.y
+        Traceback (most recent call last):
+        ...
+        AttributeError: can't delete attribute
+
+        :return: coordinate of type float
+        """
+        return self.__y
+
+    def distance(self, other=None) -> float:
+        """
+        Calculate distance between two points.
+        Note: If no point provided, function will return distance from (0, 0) point.
+        >>> a = Point(1, 0)
+        >>> b = Point(6, 0)
+        >>> a.distance(b)
+        5.0
+        >>> a.distance()
+        1.0
+        >>> a.distance('foo')
+        Traceback (most recent call last):
+        ...
+        ValueError: Wrong type of argument. Distance is calculated between two Point objects.
+
+       :param other: Point to calculate distance to.
+       :type other: Point instance.
+       :return: float value of distance
+       """
+        if isinstance(other, Point):
+            return math.hypot(self.x - other.x, self.y - other.y)
+        elif other is None:
+            return math.hypot(self.x, self.y)
+        else:
+            raise ValueError('Wrong type of argument. Distance is calculated between two Point objects.')
+
+    @staticmethod
+    def on_one_line(*args) -> bool:
+        """
+        This method checks belonging of all given points to one line.
+        Note: If 1 or 2 points are given function will always return True because every single point belongs to some
+        line as well as every two points.
+        Examples:
+        >>> a = Point(1, 0)
+        >>> b = Point(34, 0)
+        >>> c = Point(42, 0)
+        >>> Point.on_one_line(a, b, c)
+        True
+        >>> a = Point(1, 2)
+        >>> b = Point(34, 43)
+        >>> c = Point(42, 54)
+        >>> Point.on_one_line(a, b, c)
+        False
+        >>> Point.on_one_line(a)
+        True
+        >>> Point.on_one_line(Point(1, 3), Point(32, 34))
+        True
+        >>> Point.on_one_line('foo', a , b, c)
+        Traceback (most recent call last):
+        ...
+        ValueError: Some of given arguments is not points.
+
+        :param args: Any number of points.
+        :type args: Point instances.
+        :return: True if all given points lie on one straight line. False otherwise.
+        """
+        if not all(isinstance(arg, Point) for arg in args):
+            raise ValueError('Some of given arguments is not points.')
+        elif len(args) <= 1:
+            # One point as well as two points always belong to one line, thus:
+            return True
+        else:
+            check_three_points = partial(lambda a, b, c: (a.x - b.x) * (a.y - c.y) == (a.x - c.x) * (a.y - b.y),
+                                         args[0], args[1])
+            return all(map(check_three_points, args[2:]))
+
 
 class Triangle:
     """
-    Represents triangle, defined by three points.
+    Represents triangle, defined by three points a, b and c.
+                         a
+                        /\
+                       /  \
+                    c /____\ b
+    Example:
+    >>> t = Triangle(Point(0, 0), Point(8, 0), Point(4, 5))
+    >>> t
+    ((0.0, 0.0), (8.0, 0.0), (4.0, 5.0))
+    >>> t.area()
+    20.0
+    >>> t.is_equilateral
+    False
+    >>> t.is_isosceles
+    True
     """
-    def __init__(self, *args):
-        """
-        This method takes exactly three points to initialise a triangle.
-        :param args: Tuples that define
-        :type args:
-        """
-        if self.__points_checker(*args):
-            self.points = args
 
-    def __points_checker(self, *args):
+    def __init__(self, a=None, b=None, c=None):
         """
-        Check that points is really form a triangle.
+        This method takes three points to initialise a triangle.
+        Given points define triangle vertex.
 
-        :param args: points in form of tuples e.g. (1, 2), (4, 8), (12, 23)
-        :return: boolean True -- if points can define a triangle, False otherwise.
+        Example:
+        >>> Triangle(Point(0, 0), Point(0, 3), Point(4, 4))
+        ((1.0, 1.0), (2.0, 3.0), (4.0, 5.0))
+        >>> Triangle(Point(1, 1), Point(1, 3), Point(1, 5))
+        Traceback (most recent call last):
+        ...
+        ValueError: Given points belong to one line and can not define triangle.
+
+        :param a:
+        :type a:
+        :param b:
+        :type b:
+        :param c:
+        :type c:
         """
-
-        # Check that exactly three points given
-        # And each point have two coordinates
-        # Also that coordinates is int or float
-        # Then check that given points is not the same
-        # Whether points belong to one line
-        if len(args) != 3:
-            raise AttributeError('Wrong number of points. Must be 3, given {}'.format(len(args)))
-        elif not all(isinstance(component, (int, float)) for point in args for component in point):
-            raise AttributeError('Wrong point components. Point must be defined with int or float numbers.')
-        elif any(len(arg) != 2 for arg in args):
-            raise AttributeError('Wrong number of coordinates in point. Must be exactly two coordinates in each point.')
-        elif len(set(args)) != 3:
-            raise AttributeError('It seems that some points are the same. Coincident points can not define triangle.')
-        elif self.__on_one_line(*args):
-            raise AttributeError('Given points are belong to one line. Such points can not define triangle.')
+        if not any((a, b, c)) and not Point.on_one_line(a, b, c):
+            self._a = a
+            self._b = b
+            self._c = c
         else:
-            return True
+            raise ValueError('Given points belong to one line and can not define triangle.')
 
-    @staticmethod
-    def __on_one_line(*args):
+    def __repr__(self):
+        return '({}, {}, {})'.format(self._a, self._b, self._c)
+
+    def area(self, mantissa=3):
         """
-        Function to check if three points belong to one line.
+        Method to calculate area of triangle. Calculated value is rounded according to mantissa parameter.
+        Mantisssa define how much digits after decimal point to be left.
+        By default mantissa=3. If more accurate value is necessary increase mantissa.
+        If mantissa less than zero than function return 0.0
+        Example:
+        >>> t = Triangle(Point(0, 3.1415), Point(2.7, 3), Point(3 ** 0.5, 6.023))
+        >>> t.area(1)
+        4.0
+        >>> t.area()
+        4.013
+        >>> t.area(6)
+        4.012568
+        >>> t.area(-1)
+        Traceback (most recent call last):
+        ...
+        ValueError: Negative number for mantissa is not allowed.
 
-        :param args: Points to check.
-        :type args: Three tuples of two integers in each. E.g. args = (1, 2), (3, 4), (5, 6)
-        :return: True if given points belong to one line. False otherwise.
+        :param mantissa: Amount of numbers after decimal point.
+        :type mantissa: int
+        :return:
         """
-        p1, p2, p3 = args
-        return (p1[0] - p3[0]) * (p2[1] - p3[1]) == (p1[1] - p3[1]) * (p2[0] - p3[0])
+        if mantissa < 0:
+            raise ValueError('Negative number for mantissa is not allowed.')
+        a_edge = self._a.distance(self._b)
+        b_edge = self._b.distance(self._c)
+        c_edge = self._c.distance(self._a)
+        p = (a_edge + b_edge + c_edge) / 2
+        return round(math.sqrt(p * (p - a_edge) * (p - b_edge) * (p - c_edge)), mantissa)
 
-    def area(self):
-        a, b, c = [math.hypot(*map(lambda elem: elem[1] - elem[0], zip(*pair))) for pair in cb(self.points, 2)]
-        p = (a + b + c) / 2
-        return math.sqrt(p * (p - a) * (p - b) * (p - c))
+    @property
+    def is_isosceles(self):
+        """
+        Checks whether triangle is isosceles.
+        Example:
+        >>> a = Point(-2, 0)
+        >>> b = Point(2, 0)
+        >>> c = Point(0, 4)
+        >>> t = Triangle(a, b, c)
+        >>> t.is_isosceles
+        True
+
+        :return: True if triangle have at least two equal edges. False otherwise.
+        """
+        a_edge = self._a.distance(self._b)
+        b_edge = self._b.distance(self._c)
+        c_edge = self._c.distance(self._a)
+        return any([a_edge == b_edge, b_edge == c_edge, c_edge == a_edge])
+
+    @property
+    def is_equilateral(self) -> bool:
+        """
+        Checks if triangle is equilateral.
+        >>> a = Point(-9, 10)
+        >>> b = Point(-1, 4)
+        >>> c = Point(3 * 3 ** 0.5 - 5, 4 * 3 ** 0.5 + 7)
+        >>> t = Triangle(a, b, c)
+        >>> t.is_equilateral
+        True
+
+        :return: True if edges of triangle are equal. False otherwise.
+        """
+        a_edge = self._a.distance(self._b)
+        b_edge = self._b.distance(self._c)
+        c_edge = self._c.distance(self._a)
+        return all([a_edge == b_edge, b_edge == c_edge, c_edge == a_edge])
+
 
 if __name__ == '__main__':
+    t = Triangle(Point(0, 0), Point(0, 8), Point(4, 5))
+    t.area()
     import doctest
-    doctest.testmod()
+    #doctest.testmod()
---
+++ hw_11/unit_test_tests.py
@@ -0,0 +1,188 @@
+import unittest
+from triangle_area import Point, Triangle
+
+class ClassPointTest(unittest.TestCase):
+    """Test case for class Point in triangle_area.py."""
+
+    def test_point_positive_init_no_arguments(self):
+        """Positive test of __init__ function without arguments."""
+        p = Point()
+        self.assertEqual(p.x, 0.0, 'Test of Point().x failed. Returned value != 0.0')
+        self.assertEqual(p.y, 0.0, 'Test of Point().y failed. Returned value != 0.0')
+
+    def test_point_positive_init_one_argument(self):
+        """Positive test of __init__ function with one given argument."""
+        p = Point(3)
+        self.assertEqual(p.x, 3.0, 'Test of Point(3).x failed. Returned value != 3.0')
+        self.assertEqual(p.y, 0.0, 'Test of Point(3).y failed. Returned value != 0.0')
+
+    def test_point_positive_init_x_keyword_argument(self):
+        """Positive test of __init__ function with one given argument x as keyword argument."""
+        p = Point(x=4)
+        self.assertEqual(p.x, 4.0, 'Test of Point(x=4).x failed. Returned value != 4.0')
+        self.assertEqual(p.y, 0.0, 'Test of Point(x=4).y failed. Returned value != 0.0')
+
+    def test_point_positive_init_y_keyword_argument(self):
+        """Positive test of __init__ function with one given argument y as keyword argument."""
+        p = Point(y=5)
+        self.assertEqual(p.x, 0.0, 'Test of Point(y=5).x failed. Returned value != 0.0')
+        self.assertEqual(p.y, 5.0, 'Test of Point(y=5).y failed. Returned value != 5.0')
+
+    def test_point_positive_init_correct_arguments(self):
+        """Positive test of __init__ function with both correct arguments."""
+        p = Point(2, 23.52)
+        self.assertEqual(p.x, 2.0, 'Test of Point(2, 23.52).x failed. Returned value != 2.0')
+        self.assertEqual(p.y, 23.52, 'Test of Point(2, 23.52).y failed. Returned value != 23.52')
+
+        p = Point(x=2, y=23.52)
+        self.assertEqual(p.x, 2.0, 'Test of Point(x=2, y=23.52).x failed. Returned value != 2.0')
+        self.assertEqual(p.y, 23.52, 'Test of Point(x=2, y=23.52).y failed. Returned value != 23.52')
+
+        p = Point(y=2, x=23.52)
+        self.assertEqual(p.y, 2.0, 'Test of Point(y=2, x=23.52).x failed. Returned value != 23.52')
+        self.assertEqual(p.x, 23.52, 'Test of Point(y=2, x=23.52).y failed. Returned value != 2.0')
+
+    def test_point_negative_init_wrong_type_argument(self):
+        """
+        Negative test of __init__ function with one given argument of wrong types.
+        Checked argument types -> str, list, tuple, dict, set
+        """
+        with self.assertRaises(ValueError) as err:
+            Point('five', 'three')
+            self.assertEqual(err.args[0], 'Wrong value to define point. Both x and y must be int or float.',
+                             "Test of Point('five', 'three') failed.")
+
+        with self.assertRaises(ValueError) as err:
+            Point(['five', 'two'])
+            self.assertEqual(err.args[0], 'Wrong value to define point. Both x and y must be int or float.',
+                             "Test of Point(['five', 'two']) failed.")
+
+        with self.assertRaises(ValueError) as err:
+            Point([5, 2])
+            self.assertEqual(err.args[0], 'Wrong value to define point. Both x and y must be int or float.',
+                             "Test of Point([5, 2]) failed.")
+
+        with self.assertRaises(ValueError) as err:
+            Point((1, 3))
+            self.assertEqual(err.args[0], 'Wrong value to define point. Both x and y must be int or float.',
+                             "Test of Point((1, 3)) failed.")
+
+        with self.assertRaises(ValueError) as err:
+            Point({'x': 3, 'y': 0.1})
+            self.assertEqual(err.args[0], 'Wrong value to define point. Both x and y must be int or float.',
+                             "Test of Point({'x': 3, 'y': 0.1}) failed.")
+
+        with self.assertRaises(ValueError) as err:
+            Point({3, 0.1})
+            self.assertEqual(err.args[0], 'Wrong value to define point. Both x and y must be int or float.',
+                             "Test of Point({3, 0.1}) failed.")
+
+    def test_point_positive_repr(self):
+        """Positive test of __repr__ method."""
+        p = Point(x=3, y=5)
+        self.assertEqual(p.__repr__(), '(3.0, 5.0)',
+                         'Test of Point(x=3, y=5).__repr__() failed. Returned value != (3.0, 5.0)')
+
+    def test_point_positive_add(self):
+        """Positive test of __add__ method."""
+        p1 = Point(x=3, y=5)
+        p2 = Point(5, 3)
+        p = p1 + p2
+        self.assertEqual(p.__repr__(), '(8.0, 8.0)',
+                         'Test of Point(x=3, y=5) + Point(5, 3) failed. Returned value != (8.0, 8.0)')
+
+    def test_point_negative_x_and_y_setting_deleting(self):
+        """Negative test of attribute setting and deleting method."""
+        p = Point(x=6.67, y=5.34)
+
+        with self.assertRaises(AttributeError) as err:
+            p.x = 9
+            self.assertEqual(err.args[0], "Can't set attribute.",
+                             "Test of Point(x=6.67, y=5.34).x = 9 failed, no AttributeError was raised.")
+
+        with self.assertRaises(AttributeError) as err:
+            p.y = 10
+            self.assertEqual(err.args[0], "Can't set attribute.",
+                             "Test of Point(x=6.67, y=5.34).y = 10 failed, no AttributeError was raised.")
+
+        with self.assertRaises(AttributeError) as err:
+            del p.x
+            self.assertEqual(err.args[0], "Can't delete attribute.",
+                             "Test of del Point(x=6.67, y=5.34).x failed, no AttributeError was raised.")
+
+        with self.assertRaises(AttributeError) as err:
+            del p.y
+            self.assertEqual(err.args[0], "Can't delete attribute.",
+                             "Test of del Point(x=6.67, y=5.34).y failed, no AttributeError was raised.")
+
+    def test_point_positive_x_and_y_getting(self):
+        """Positive test of attribute getting."""
+        p = Point(x=6.67, y=5.34)
+        self.assertEqual(p.x, 6.67, "Test of Point(x=6.67, y=5.34).x failed, returned value != 6.67.")
+        self.assertEqual(p.y, 5.34, "Test of Point(x=6.67, y=5.34).y failed, returned value != 5.34.")
+
+    def test_point_positive_distance(self):
+        """Positive test of Point.distance method."""
+        p1 = Point(1, 7)
+        p2 = Point(4, 23)
+        self.assertEqual(p1.distance(p2), 265 ** 0.5,
+                         "Test of Point(1, 7).distance(Point(4, 23)) failed, returned value != 16.278820596099706.")
+        self.assertEqual(p1.distance(), 5 * 2 ** 0.5,
+                         "Test of Point(x=6.67, y=5.34).distance() failed, returned value != 7.0710678118654755.")
+
+    def test_point_negative_distance(self):
+        """Negative test of Point.distance method."""
+
+        with self.assertRaises(ValueError) as err:
+            Point(1, 7).distance('foo')
+            self.assertEqual(err.args[0], "Wrong type of argument. Distance is calculated between two Point objects.",
+                         "Test of Point(1, 7).distance('foo') failed, no ValueError was raised.")
+
+    def test_point_positive_on_one_line(self):
+        """Negative test of Point.on_one_line method."""
+        a = Point(1, 0)
+        b = Point(34, 0)
+        c = Point(42, 0)
+
+        self.assertEqual(Point.on_one_line(a, b, c), True,
+                         "Test of Point.on_one_line(a, b, c) failed, returned value != True.")
+        d = Point(1, 2)
+        e = Point(34, 43)
+        f = Point(42, 54)
+
+        self.assertEqual(Point.on_one_line(d, e, f), False,
+                         "Test of Point.on_one_line(d, e, f) failed, returned value != False.")
+
+        self.assertEqual(Point.on_one_line(a), True,
+                         "Test of Point.on_one_line(a) failed, returned value != True.")
+
+    def test_point_negative_on_one_line(self):
+        """Negative test of Point.on_one_line method."""
+        a = Point(1, 0)
+        b = Point(34, 0)
+        c = Point(42, 0)
+
+        with self.assertRaises(ValueError) as err:
+            Point.on_one_line('foo', a, b, c)
+            self.assertEqual(err.args[0], "Some of given arguments is not points.",
+                             "Test of Point.on_one_line('foo', a, b, c) failed, no ValueError was raised.")
+
+
+    def test_triangle_positive_init(self):
+        """Positive test of Triangle.__init__ method."""
+        t = Triangle(Point(1, 1), Point(2, 3), Point(4, 5))
+        self.assertEqual(t.__repr__(), '((1.0, 1.0), (2.0, 3.0), (4.0, 5.0))',
+                         "Test of Triangle(Point(0, 0), Point(8, 0), Point(4, 5)) failed,\
+                          returned value != ((1.0, 1.0), (2.0, 3.0), (4.0, 5.0)).")
+
+    def test_triangle_negative_init_points_on_line(self):
+        """Negative test of Triangle.__init__ method. Points belong to one line."""
+        with self.assertRaises(ValueError) as err:
+            Triangle(Point(1, 1), Point(1, 3), Point(1, 5))
+            self.assertEqual(err.args[0], "Given points belong to one line and can not define triangle.",
+                             "Triangle(Point(1, 1), Point(1, 3), Point(1, 5)) failed, no ValueError was raised.")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
