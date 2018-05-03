e3a691f73d8f5c343ba859cd2a0b28e2162bf296
fag <fedoseevalex@inbox.ru>
2018-05-03 22:03:56 +0300

    Transfer commit

---
+++ hw_11/CommandLine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/ python3.6
+# HW #L11.2 Triangle area
+
+
+"""
+Command line module implements user text interface.
+
+"""
+
+def get_cmd():
+    """
+    Function to get
+    :return:
+    """
