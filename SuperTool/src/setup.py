66ff2331c142c10626b58c6d0dfa808939b14b6e
fag <fedoseevalex@inbox.ru>
2018-05-06 12:38:45 +0300

    SuperTool added

---
+++ SuperTool/src/setup.py
@@ -0,0 +1,13 @@
+# License MIT
+
+from setuptools import setup
+
+setup(
+    name="supertool_distro",
+    version='0.1',
+    description='Super super tool',
+    author='FAG',
+    author_email='fedoseevalex@inbox.ru',
+    license='MIT',
+
+)
