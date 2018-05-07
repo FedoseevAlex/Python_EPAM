781f898852c4486bed043d3fe37c1978a7a3f31c
fag <fedoseevalex@inbox.ru>
2018-05-07 14:56:01 +0300

    Logic set up.

---
+++ SuperTool/src/SuperTool/find.py
@@ -0,0 +1,62 @@
+import os
+from contextlib import closing
+
+
+def similar(**kwargs):
+    """
+    Entry point for similar_files script.
+    This function handles hash calculation of all files found in requested directory (if -r option is enabled
+    then also in subdirectories). Hash calculated from file contents. After that duplicate files are printed.
+
+    :param kwargs: Command line options got from user as keywords. Contains directory to find and other options.
+    :type kwargs: dict
+    :return: None
+    """
+    # Create directory paths generator
+    dirpath_generator = os.walk(kwargs['directory'])
+    index_files = dict()
+    while True:
+        # Get next value from dirnames_generator
+        try:
+            contents = next(dirpath_generator)
+        except StopIteration:
+            break
+        else:
+            current_dir, sub_dirs, files = contents
+
+        for file in files:
+            file_path = os.path.join(current_dir, file)
+            with closing(open(file_path, 'rb')) as f:
+                short_path = os.path.join(os.path.basename(current_dir), file)
+                file_hash = hash(f.read())
+                if file_hash in index_files.keys():
+                    index_files[file_hash].append(short_path)
+                else:
+                    index_files[file_hash] = [short_path, ]
+        # Exit if -r flag is not present
+        if not kwargs['recursively']:
+            break
+    for key in index_files.copy().keys():
+        if len(index_files[key]) < 2:
+            index_files.pop(key)
+
+    print_work(index_files)
+
+
+def print_work(duplicates: dict):
+    """
+    Handles output for user.
+
+    :param duplicates: Contains groups of similar files by file hash.
+    :type duplicates: dict -- keys: file hash, values: list of similar files
+    :return: None
+    """
+    if duplicates:
+        print('Duplicate files found:\n')
+        for index, files in enumerate(duplicates.values(), start=1):
+            print('\tSimilar group {}\n'.format(index))
+            print(*files, sep='\n')
+            print(r'*' * 30, end='\n\n')
+    else:
+        print('No similar files found!')
+
--- SuperTool/src/setup.py
+++ SuperTool/src/setup.py
@@ -1,6 +1,21 @@
 # License MIT
 
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+
+DISTRO_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def extract_requirements(file):
+    """
+    Extract requirements from requirements file.
+
+    :param file: path to requirements file
+    :type file: str
+    :return: list[str] -- list of requirements
+    """
+    with open(file, 'r') as file:
+        return file.read().splitlines()
 
 setup(
     name="supertool_distro",
@@ -9,5 +24,14 @@ setup(
     author='FAG',
     author_email='fedoseevalex@inbox.ru',
     license='MIT',
-
+    classifiers=[
+        'Topic :: Education',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['tests']),
+    install_requires=extract_requirements(os.path.join(DISTRO_ROOT_PATH, 'requirements', 'base.txt')),
+    test_requires=extract_requirements(os.path.join(DISTRO_ROOT_PATH, 'requirements', 'test.txt')),
+    test_suite='nose.collector',
+    scripts=[os.path.join('bin', 'similar_files')],
+    zip_safe=False
 )
